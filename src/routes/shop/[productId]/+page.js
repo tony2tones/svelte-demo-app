43459df890ec5da868082b45d43f4195996309da
tony <anthonymarques@outlook.com>
2023-09-28 07:58:10 +0200

    added dynamic routing

---
+++ src/routes/shop/[productId]/+page.js
@@ -0,0 +1,12 @@
+export const load = ({fetch, params}) => {
+console.log(params)
+
+const fetchProduct = async (id) => {
+    const res = await fetch(`https://dummyjson.com/products/${id}`)
+    const data = res.json();
+    return data
+}
+return {
+    product: fetchProduct(params.productId)
+}
+}
